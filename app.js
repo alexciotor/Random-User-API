fbbc3c7fa9d84d24ed2497b8f04d0d2057f5fb72
alexciotor <ciotor.alex@yahoo.com>
2020-11-16 10:35:46 +0200

    First Take on this project

---
+++ app.js
@@ -0,0 +1,70 @@
+const img = document.querySelector('#image');
+const text = document.querySelector('#text');
+const name = document.querySelector('#name');
+const spans = document.querySelectorAll('.span');
+const  url= 'https://randomuser.me/api/'
+ const button = document.querySelector('.randome')
+
+ 
+ 
+
+    button.addEventListener('click', async()=>{
+        try{
+      const data = await fetch(url);
+      const response = await data.json()
+ const person = response.results[0]
+ 
+const {
+    name:{first},
+    name:{last},
+    picture:{large},
+    email,
+    dob:{age},
+    phone,
+    login:{
+    password },
+location:{
+    street:{name:streetName},
+    street:{number:StreetNumber}
+}
+}=person 
+img.src =large
+text.textContent = 'My Name is '
+name.textContent = `${first} ${last}`
+spans.forEach(btn=>{
+    btn.addEventListener('click',()=>{
+ if(btn.classList.contains('name')){
+     text.textContent = 'My Name is '
+    name.textContent = `${first} ${last}`
+}
+if(btn.classList.contains('email')){
+    text.textContent= 'My email is'
+    name.textContent =email
+}
+if(btn.classList.contains('street')){
+text.textContent= 'My Street is'
+    name.textContent = `${streetName} ${StreetNumber }`
+}
+if(btn.classList.contains('age')){
+    text.textContent = 'My Age is '
+    name.textContent = age
+}
+if(btn.classList.contains('phone')){
+    text.textContent = 'My phone is ';
+    name.textContent = phone
+}
+if(btn.classList.contains('password')){
+    text.textContent = 'My Passwonrd is '
+    name.textContent = password
+}
+    })
+})
+        }
+        catch(error){
+console.log(error)
+        }
+    })
+ 
+ 
+
+
